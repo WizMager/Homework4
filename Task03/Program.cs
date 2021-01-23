79f71ca8e476ff3dee259ca23cd0a70cb1eaa17c
Anton <wizmager@gmail.com>
2021-01-23 13:11:50 +0300

    Add project files.

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    // 3. Решить задачу с логинами из предыдущего урока, только логины и пароли считать из файла в массив.Создайте структуру Account, содержащую Login и Password.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
